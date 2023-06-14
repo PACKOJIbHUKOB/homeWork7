a38d6d2c44d0d4afd25c8ef744881fbb5fa39c54
PACKOJIbHUKOB <jordn-5@yandex.ru>
2023-06-14 17:34:57 +0300

    Шаблон домашнего задания 7

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {   task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();}
+    private static void task1() {
+        System.out.println("задача 1");}
+    private static void task2() {
+        System.out.println("задача 2");
+
+    }
+    private static void task3() {
+        System.out.println("задача 3");}
+    private static void task4() {
+        System.out.println("задача 4");}
+
+    private static void task5() {
+        System.out.println("задача 5");}
+
+    private static void task6() {
+        System.out.println("задача 6");}
+
+    private static void task7() {
+        System.out.println("задача 7");}
+
+    private static void task8() {
+        System.out.println("задача 8");}
+}
